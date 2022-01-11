817f51bf6cca41b0365175afc50b855d6d6e27d6
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-01-11 15:54:58 -0700

    Update roadmap.js

--- src/roadmap.js
+++ src/roadmap.js
@@ -1,6 +1,50 @@
 /*
   Things to do.
-  Canvas.
+  * Canvas.
+  
   Input.
-  Basic object functionality.
+    Keyboard,
+    Mouse,
+      X,
+      Y,
+      Left,
+      Right,
+      Middle,
+      Double
+      
+  Basic object functionality,
+    Update,
+    Start,
+    Exit
+    
+  Rectangles,
+    Position,
+    Size,
+    Color
+    
+  Images,
+    Rectangle,
+    Image URL
+    
+  Text,
+    Rectangle,
+    Size,
+    Type,
+    Text,
+    Best Fit,
+    Italic,
+    Bold,
+    Underline
+    
+  Oval,
+    Rectangle,
+    Color
+    
+  SVG,
+    Path2D
+    
+  Collision,
+    OnEnter,
+    OnThisFrame,
+    OnExit
 */
