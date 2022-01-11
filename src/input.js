9ec2ce643defc00dfd260fd21929beceb8382de1
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-01-11 16:06:59 -0700

    Create input.js

---
+++ src/input.js
@@ -0,0 +1,34 @@
+import * from 'core.js'
+var Keys = {};
+window.onkeydown = (e) => {
+  Keys[e.key.toLowerCase()] = true;
+}
+
+window.onkeyup = (e) => {
+  Keys[e.key.toLowerCase()] = false;
+}
+
+var Mouse = {};
+window.onblur = () => {
+  for(var i of _keys) {
+    i = false;
+  }
+  
+  for(var i of Mouse) {
+    i = false;
+  }
+}
+
+var _mouseLut = [
+  "left",
+  "middle',
+  "right"
+];
+
+window.onmousedown = (e) => {
+  Mouse[_mouseLut[e.which]] = true;
+}
+
+window.onmouseup = () => {
+  Mouse[_mouseLut[e.which]] = false;
+}
