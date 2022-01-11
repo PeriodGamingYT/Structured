b93ca7cd08cbbc3726f75fb0333cd8ccb91f408d
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-01-11 13:06:08 -0700

    Update canvas.js
    Optional size and now an ActionEnum

--- src/canvas.js
+++ src/canvas.js
@@ -1,11 +1,15 @@
-function Canvas() {
+import * from 'core.js';
+
+function Canvas(width=window.innerWidth, height=window.innerHeight) {
   var canvas = document.createElement("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
   canvas.style.position = "absolute";
   canvas.style.left = "0";
   canvas.style.top = "0";
+  canvas.style.width = "auto";
+  canvas.style.height = "auto";
   document.body.appendChild(canvas);
   canvas = canvas.getContext("2d");
-  return canvas;
+  return _ActionEnum("canvas", canvas, width, height);
 }
