c2c804bf694851fca96c45915f097e1e0294812c
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-01-13 16:40:49 -0700

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,28 +1,32 @@
-export function _ActionEnum(name) {
-  return {
-    name: name,
-    args: args
-  };
+function _ActionEnum(name) {
+	var args = Array.from(arguments);
+	var args = [...args];
+	return {
+		name: name,
+		args: args.slice(1),
+		found: true
+	};
 }
 
-export function _FindEnum(name, args, required=false) {
-  for(var i of args) {
-    if(i.name == name) {
-      return {
-        name: i.name,
-        found: true,
-        args: i.args
-      }
-    }
-  }
-  
-  if(required) {
-    throw new Error(`Property ${name}() is missing.`);
-  } else {
-    return {
-      name: null,
-      found: false,
-      args: null
-    }
-  }
-} 
+function _FindEnum(name, args, required=false) {
+	for(var i of args) {
+		if(i.name == name && i.found) {
+			return {
+				name: i.name,
+				found: true,
+				args: i.args
+			}
+		}
+	}
+	
+	if(required) {
+		throw new Error(`Property ${name}() is missing.`);
+	} else {
+		// Not null so _FindEnum() doesn't bug out.
+		return {
+			name: "",
+			found: false,
+			args: []
+		};
+	}
+}
