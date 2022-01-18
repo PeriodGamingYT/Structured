64befae3f623335e9aa58b896b23bbe674b407e7
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-01-18 16:50:39 -0700

    Update input.mjs

--- src/input.mjs
+++ src/input.mjs
@@ -1,3 +1,12 @@
+export var Focused = true;
+window.onblur = () => {
+  Focused = false;
+}
+
+window.onfocus = () => {
+	Focused = true;
+}
+
 export var Keys = {};
 window.onkeydown = (e) => {
   Keys[e.key.toLowerCase()] = true;
@@ -7,27 +16,61 @@ window.onkeyup = (e) => {
   Keys[e.key.toLowerCase()] = false;
 }
 
+import { Vector2 } from './vector2.mjs';
 export var Mouse = {};
-window.onblur = () => {
-  for(var i of _keys) {
-    i = false;
-  }
-  
-  for(var i of Mouse) {
-    i = false;
-  }
-}
 
 var _mouseLut = [
   "left",
+  "right",
   "middle",
-  "right"
+  "back",
+  "forward"
 ];
 
-window.onmousedown = (e) => {
-  Mouse[_mouseLut[e.which]] = true;
+function decode(buttons, mouseLut=false) {
+	if(buttons === 0) {
+		return mouseLut ? _mouseLut : [];
+	}
+	
+	var result = [];
+	var i = 1;
+	var j = 0;
+	while(i <= 16) {
+		if(buttons & i === i) {
+			result.push(_mouseLut[j]);
+		}
+
+		j++;
+		i *= 2;
+	}
+	
+	return result;
 }
 
-window.onmouseup = () => {
-  Mouse[_mouseLut[e.which]] = false;
+window.onmousedown = (e) => {
+	var buttons = decode(e.buttons);
+  for(var i of buttons) {
+  	Mouse[i] = true;
+  }
+}
+
+window.onmouseup = (e) => {
+  var buttons = decode(e.buttons, true);
+  for(var i of buttons) {
+  	Mouse[i] = false;
+  }
+}
+
+window.ondblclick = () => {
+	Mouse["double"] = true;
+}
+
+var timeout;
+window.onmousemove = (e) => {
+	clearTimeout(timeout);
+	Mouse["where"] = new Vector2(e.clientX, e.clientY);
+	Mouse["move"] = new Vector2(e.movementX, e.movementY);
+	timeout = setTimeout(function() {
+		Mouse["move"] = new Vector2(0, 0);
+	}, 1);
 }
