1e87f0a987c87552626795b4077550c13e374e1e
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-01-26 19:47:38 -0700

    removed some test demo code.

--- Structured.mjs
+++ Structured.mjs
@@ -1,8 +1,5 @@
 import { _FindEnum, _ActionEnum } from './src/core.mjs';
-import { Canvas, CanvasPath } from './src/canvas.mjs';
-import { Text, TextString, Bold, DrawText } from './src/text.mjs';
-import { Rectangle } from './src/rectangle.mjs';
-import { Position, Size } from './src/vector2.mjs';
+import { Canvas } from './src/canvas.mjs';
 
 export function Game() {
 	var args = Array.from(arguments);
@@ -10,18 +7,6 @@ export function Game() {
 	var canvas = Canvas(
 		_FindEnum("position", arguments, false),
 		_FindEnum("size", arguments, false)
-	);
-
-	DrawText(
-		canvas,
-		Text(
-			TextString("Hello, World!"),
-			Bold(true),
-			Rectangle(
-				Position(50, 50),
-				Size(50, 50)
-			)
-		)
 	);
 
 	return _ActionEnum("game", canvas);
