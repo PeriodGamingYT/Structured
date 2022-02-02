aa5545d69efdf7d188f77879e5333a03ec5cabfb
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-02-01 19:06:37 -0700

    i forgot to remove test includes

--- Structured.mjs
+++ Structured.mjs
@@ -1,7 +1,5 @@
 import { _FindEnum, _ActionEnum } from './src/core.mjs';
 import { Canvas } from './src/canvas.mjs';
-import { CanvasDraw } from './src/object.mjs';
-import { CImage, URL } from './src/image.mjs';
 
 export function Game() {
 	var args = Array.from(arguments);
